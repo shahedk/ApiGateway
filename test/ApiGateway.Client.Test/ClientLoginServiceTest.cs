e0d6e44930a872b2e750b74b02a3694f52aa26a7
shahedk <shahedk@gmail.com>
2018-12-06 11:52:08 +0600

    Adding unit tests

---
+++ test/ApiGateway.Client.Test/ClientLoginServiceTest.cs
@@ -0,0 +1,15 @@
+using Xunit;
+
+namespace ApiGateway.Client.Test
+{
+    
+    public class ClientLoginServiceTest
+    {
+        [Fact]
+        public void Test1()
+        {
+            
+        }
+        
+    }
+}
---
+++ test/ApiGateway.InternalClient.Test/InternalClientLoginServiceTest.cs
@@ -0,0 +1,81 @@
+
+using Xunit;
+
+
+namespace ApiGateway.InternalClient.Test
+{
+    public class InternalClientLoginServiceTest 
+    {
+        [Fact]
+        public void ServiceWithNoApiName_HttpGet()
+        {
+            //var service = new InternalClientLoginService();
+        }
+        
+        [Fact]
+        public void ServiceWithNoApiName_HttpPut()
+        {
+            
+        }
+        
+        [Fact]
+        public void ServiceWithNoApiName_HttpPost()
+        {
+            
+        }
+        
+        [Fact]
+        public void ServiceWithNoApiName_HttpDelete()
+        {
+            
+        }
+        
+        [Fact]
+        public void ApiNameWithNoParameter_HttpGet()
+        {
+            
+        }
+        
+        [Fact]
+        public void ApiNameWithNoParameter_HttpPut()
+        {
+            
+        }
+        
+        [Fact]
+        public void ApiNameWithNoParameter_HttpPost()
+        {
+            
+        }
+        
+        [Fact]
+        public void ApiNameWithNoParameter_HttpDelete()
+        {
+            
+        }
+        
+        [Fact]
+        public void ApiNameWithParameter_HttpGet()
+        {
+            
+        }
+        
+        [Fact]
+        public void ApiNameWithParameter_HttpPut()
+        {
+            
+        }
+        
+        [Fact]
+        public void ApiNameWithParameter_HttpPost()
+        {
+            
+        }
+        
+        [Fact]
+        public void ApiNameWithParameter_HttpDelete()
+        {
+            
+        }
+    }
+}
