991ef6cb86bc6dba3f4a778edf728fde74b78442
shahedk <shahedk@gmail.com>
2018-12-08 03:03:56 +0600

    Fixing Url parsing to properly detect service and api name

--- src/ApiGateway.Common/Extensions/UriHelper.cs
+++ src/ApiGateway.Common/Extensions/UriHelper.cs
@@ -1,18 +1,60 @@
+using System.Net;
+using ApiGateway.Common.Constants;
+using ApiGateway.Common.Exceptions;
 using Microsoft.AspNetCore.Http;
 
 namespace ApiGateway.Common.Extensions
 {
     public static class UriHelper
     {
+        private static string GetPath(HttpRequest request)
+        {
+            var startIndex = request.Path.Value.IndexOf("://");
+            var path = request.Path.Value.Substring(startIndex+2).Trim();
+         
+            if (path.EndsWith("/"))
+            {
+                // Remove the trailing "/"
+                path = path.Substring(0, path.Length - 1);
+            }
+
+            return path.ToLower();
+        }
+        
         public static string GetApiName(this HttpRequest request) 
         {
             var apiNameFromPath = "";
-            var tokens = request.Path.Value.Split("/");
-        
-            if (tokens.Length > 2)
+            var path = GetPath(request);
+            
+            var tokens = path.Split("/");
+
+            if (tokens[0] == "api")
             {
-                // http://url/service-name/api-name/id
-                apiNameFromPath = tokens[2];
+                // Application API   
+                if (tokens.Length > 2)
+                {
+                    // eg. http://api/service-name/api-name/{id}
+                    apiNameFromPath = tokens[2];
+                }
+            }
+            else if (tokens[0] == AppConstants.SysApiServiceName)
+            {
+                // System API
+                if (tokens.Length > 2)
+                {
+                    // eg. http://sys/api-name/action/{id}
+                    apiNameFromPath = tokens[1] + "/" + tokens[2];
+                }
+                else if (tokens.Length > 1)
+                {
+                    // eg. http://sys/api-name/{id}
+                    apiNameFromPath = tokens[1];
+                }
+            }
+            else
+            {
+                // Unknown Url format
+               throw new ApiGatewayException("Unknown Url format", HttpStatusCode.NotFound);
             }
             
             return apiNameFromPath;
@@ -21,12 +63,19 @@ namespace ApiGateway.Common.Extensions
         public static string GetServiceName(this HttpRequest request)
         {
             var serviceName = "";
-            var tokens = request.Path.Value.Split("/");
+            var tokens = GetPath(request).Split("/");
             if (tokens.Length > 1)
             {
                 // http://url/service-name/api-name/id
-                
-                serviceName = tokens[1];
+                if (tokens[0].ToLower() == AppConstants.SysApiServiceName)
+                {
+                    serviceName = tokens[0];
+                }
+                else if (tokens[0].ToLower() == "api")
+                {
+                    serviceName = tokens[1];
+                }
+
             }
 
             return serviceName;
